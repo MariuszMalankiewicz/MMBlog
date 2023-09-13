7f67cfccc9e42e1fbca9da2a5fadd34c5d629622
MariuszMalankiewicz <m2ario123@wp.pl>
2023-09-13 13:47:56 +0200

    add relationship post, comment, user

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -15,4 +15,10 @@ class Comment extends Model
     {
         return $this->belongsTo(Post::class);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
 }
