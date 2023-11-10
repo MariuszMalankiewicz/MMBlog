2dcb98e9656e947c09585e3649497f45245c005d
MariuszMalankiewicz <m2ario123@wp.pl>
2023-11-10 11:32:45 +0100

    update footter

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,4 @@
-
-<footer class="bg-white rounded-lg shadow dark:bg-gray-900">
+<footer class="bg-white rounded-t-lg shadow dark:bg-gray-900">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
 
@@ -7,21 +6,13 @@
 
             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
-                    <a href="https://mariusz-malankiewicz-portfolio.netlify.app" class="mr-4 hover:underline md:mr-6 ">Portfolio</a>
+                    <a href="https://portfolio-mariusz-malankiewicz.netlify.app" target="blank" class="mr-4 hover:underline md:mr-6">Portfolio</a>
                 </li>
                 <li>
-                    <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-                </li>
-                <li>
-                    <a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline">Contact</a>
-                </li>
             </ul>
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 MMBlog. All Rights Reserved.</span>
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 MMBlog. Wszystkie prawa zastrzeżone.</span>
     </div>
 </footer>
 
