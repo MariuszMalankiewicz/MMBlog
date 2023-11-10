b65c1b994b4863e3929191b46aa6baa62899297b
MariuszMalankiewicz <m2ario123@wp.pl>
2023-11-10 11:32:00 +0100

    delete archivent

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,13 +18,6 @@ class PostController extends Controller
      */
     public function index()
     {
-
-        // $posts = Post::latest()
-        // ->filter(request(['month', 'year']))
-        // ->get();
-
-
-
         $posts = Post::latest();
 
         if($month = request('month'))
@@ -39,13 +32,7 @@ class PostController extends Controller
 
         $posts = $posts->get();
 
-        $archives = Post::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
-        ->groupBy('year', 'month')
-        ->orderByRaw('min(created_at)')
-        ->get()
-        ->toArray();
-
-        return view('posts.index', compact('posts', 'archives'));
+        return view('posts.index', compact('posts'));
     }
 
     /**
