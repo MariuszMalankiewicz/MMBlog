4a556db2617230929499a875d62413c272771508
MariuszMalankiewicz <m2ario123@wp.pl>
2023-11-11 16:51:38 +0100

    update file layouts.footer.blade.php, tag a portfolio on right site

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,10 +1,10 @@
 <footer class="bg-white rounded-t-lg shadow dark:bg-gray-900">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div class="sm:flex sm:items-center sm:justify-between">
+        <div class="flex items-center justify-between">
 
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MMBlog</span>
 
-            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <ul class="flex flex-wrap items-center text-sm font-medium text-gray-500 mb-0 dark:text-gray-400">
                 <li>
                     <a href="https://portfolio-mariusz-malankiewicz.netlify.app" target="blank" class="mr-4 hover:underline md:mr-6">Portfolio</a>
                 </li>
