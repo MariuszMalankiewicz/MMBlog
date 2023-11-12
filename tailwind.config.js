8be10722a6e9de3ad2a67e4d484ee1530e0ee912
MariuszMalankiewicz <m2ario123@wp.pl>
2023-11-12 09:48:20 +0100

    delete extend screen raw height

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,7 @@ module.exports = {
   ],
   theme: {
     extend: {
-      screens: {
-        'h-800': { 'raw': '(min-height: 800px)' },
-        'h-850': { 'raw': '(min-height: 850px)' },
-        'h-900': { 'raw': '(min-height: 900px)' },
-        'h-1050': { 'raw': '(min-height: 1050px)' },
-        'h-1250': { 'raw': '(min-height: 1250px)' },
-
-      }
+  
     },
   },
   plugins: [],
